e929c49fa6072214c9ee097c21546801ab4a1482
rajapandi29 <rp6321613@gmail.com>
2026-08-24 10:37:46 +0530

    new

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,13 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "my-terraformstate-18-8"
-    key    = "my-terraformstate-18-8/buckets/"
+    key    = "./my-terraformstate-18-8/buckets/"
     region = "us-east-1"
     encrypt = true
 
-    # If you already created a DynamoDB table for state locking, keep this line.
-    # If not, either create one or remove this line (Terraform >=1.9 can use
-    # use_lockfile = true instead of DynamoDB).
-    dynamodb_table = "terraform-lock-table"
   }
 }
